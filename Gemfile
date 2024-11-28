1dd4d4491501fad5beab492c81544eed1f9639bc
Emilie Vennat <emilie.vennat@gmail.com>
2024-11-28 15:36:43 +0100

    Watch List

--- Gemfile
+++ Gemfile
@@ -59,6 +59,10 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
+gem "bootstrap", "~> 5.2"
+gem "autoprefixer-rails"
+gem "font-awesome-sass", "~> 6.1"
+gem "simple_form"
+gem "sassc-rails"
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: [:new, :create, :index, :show, :destroy] do
+    resources :movies, only: [:index, :show]
+  end
 end
